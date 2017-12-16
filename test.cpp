f5b2ce2763f460169bc0cd6ca9a8f9c9e72ad9a2
Rejdok <lvbnu1@gmail.com>
2017-12-16 11:33:32 +0300

    ttt

--- test.cpp
+++ test.cpp
@@ -3,7 +3,9 @@
 #include <boost/test/unit_test.hpp>
 
 
-BOOST_AUTO_TEST_SUITE(test_version)
+BOOST_AUTO_TEST_SUITE(test)
 
-
+BOOST_AUTO_TEST_CASE(test_fun){
+	BOOST_CHECK(true);
+}
 BOOST_AUTO_TEST_SUITE_END()
